4a2d761a06625700a91501ad0e81e13292b6a883
Yu Jia Liao <kevinc0227@gmail.com>
2021-04-03 12:00:27 +0800

    pracitce gulp

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './script.js',
+  entry: './src/js/app.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
